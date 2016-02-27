b68177001a3c251dd3f3e0a190e48dcaae57130d
Keith Hughitt <keith.hughitt@gmail.com>
2016-02-27 14:06:35 -0500

    Added some more tests relating to notebook setup and entry parsing

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,10 @@ Note: When running tests in a virtualenv container, be sure to install py.test
 """
 import os
 import pytest
-from pkg_resources import resource_filename
+from pkg_resources import resource_filename, Requirement
 from labnote.notebook import Notebook
 
 @pytest.fixture(scope="module")
 def notebook():
-    #example_conf = resource_filename(__name__,
-    #                                 os.path.join('example', 'example.config.yml'))
     example_conf = 'example/example.config.yml'
-    print(example_conf)
     return Notebook(example_conf)
--- tests/test_notebook.py
+++ tests/test_notebook.py
@@ -4,3 +4,30 @@ Notebook test code
 class TestNotebook():
     def test_load_config(self, notebook):
         assert notebook.author == 'C. Darwin'
+        assert notebook.email == 'cdarwin@geolsoc.org.uk'
+        assert notebook.title == 'Lab Notebook'
+
+        # check categories
+        assert dict(notebook.categories) == {
+                'Barnacles': ['cirripede'], 
+                'Finches': ['finch', 'natural-selection']
+        }
+
+    def test_entries(self, notebook):
+        # get a flattened list of entries
+        entries = [i[x] for i in notebook.entries.values() for x in range(len(i))]
+
+        entry_categories = [e.category for e in entries]
+        assert sorted(entry_categories) == ['Barnacles', 'Barnacles', 
+                                            'Finches', 'Finches', 'Finches',
+                                            'Other']
+
+        entry_titles = [e.title for e in entries]
+        assert sorted(entry_titles) == [
+            'Cirripede systematics analysis',
+            'Comparison of cirripede morphology',
+            'Finch Beak Size Analysis',
+            'Molothrus parasitism',
+            'foraging-strategies.py',
+            'thoughts.txt'
+        ]
